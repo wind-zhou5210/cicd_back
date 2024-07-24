07f7b3406a74ce9b2fdfbba51d3f94e663089388
周正 <zhouzheng@thwb.com.cn>
2024-07-24 11:12:31 +0800

    feat: 解决中文乱码

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ let server = http.createServer(function (req, res) {
   console.log(req.method, req.url);
   if (req.url == "/api/users") {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/json; charset=utf-8"); // 设置Content-Type
     res.end(JSON.stringify(users));
   } else {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8"); // 设置Content-Type
     res.end("Now Found!");
   }
 });
